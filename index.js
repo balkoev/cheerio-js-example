4add07de804ef83586037057d02489c9edf62ebc
Adam <adam22185515@gmail.com>
2020-06-04 00:48:10 +0300

    init

---
+++ index.js
@@ -0,0 +1,29 @@
+const cheerio = require('cheerio');
+const axios = require('axios');
+const fs = require('fs');
+
+const parse = async () => {
+  const fetchHTML = async (url) => {
+    const { data } = await axios.get(url);
+    return cheerio.load(data);
+  };
+
+  const $ = await fetchHTML(`https://kanobu.ru/games/ps-4/popular/?page=1`);
+  const pagesNumber = $('a.ui-kit-paginator--list-link').eq(-2).text();
+
+  for (let i = 1; i < pagesNumber; i++) {
+    const selector = await fetchHTML(
+      `https://kanobu.ru/games/ps-4/popular/?page=${i}`
+    );
+    selector('.c-game').each((i, element) => {
+      const title = selector(element).find('div.h2').first().text();
+      const link = `https://kanobu.ru${selector(element)
+        .find('a')
+        .attr('href')}`;
+
+      fs.appendFileSync('./file.txt', `${title};${link}\n`);
+    });
+  }
+};
+
+parse();
